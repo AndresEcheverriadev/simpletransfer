e8f90784acd0442348af32beda8b37a5d4f623bd
Andres Echeverria <andres@brandpro.cl>
2023-07-11 19:02:23 -0400

    build fix

---
+++ html/static/js/873.c5dde877.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfcr_web=self.webpackChunkfcr_web||[]).push([[873],{2873:function(e,n,c){c.r(n);var u=c(2791),f=c(7689),r=c(5579);n.default=function(e){var n=e.children;(0,u.useEffect)((function(){s()}));var c=(0,f.s0)(),t=(0,u.useContext)(r.O),s=t.checkToken;return!1===t.isLoggedIn?c("/"):n}}}]);
+//# sourceMappingURL=873.c5dde877.chunk.js.map
